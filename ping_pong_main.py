99d72269980e56ba3c1e947d27322906210a1644
Yaroslav <dirty2228@mail.ru>
2026-04-25 14:48:51 +0500

    Update ping_pong_main.py
    Создание сцены игры с синим задним фоном + создание игрового цикла

--- ping_pong_main.py
+++ ping_pong_main.py
@@ -1,1 +1,12 @@
-
+from pygame import *
+window = display.set_mode((700,500))
+display.set_caption("Пинг-Понг")
+BLUE = (0, 0, 255)
+game = True
+while game:
+    events = event.get()
+    for i in events:
+        if i.type == QUIT:
+            game = False
+    window.fill(BLUE)    
+    display.update()

e78ecf6de0f879408e5e1e63c30646d6b9987205
Yaroslav <dirty2228@mail.ru>
2026-05-02 15:08:12 +0500

    Update ping_pong_main.py
    Практически готовая версия игры "Пинг-Понг". Здесь было реализовано автоматическое перемещение мяча по сцене игры, его отскок от стен + взаимодействия мяча с ракетками. Были прописаны условия проигрыша как для первой, так и для второй ракетки, введён счётчик подбитых мячей для каждой из ракеток.

--- ping_pong_main.py
+++ ping_pong_main.py
@@ -1,8 +1,15 @@
 from pygame import *
+font.init()
+font = font.Font(None,50)
+count1 = 0
+count2 = 0
+font1 = font.render("Player 1 loses",True,(255, 0, 0))
+font2 = font.render("Player 2 loses",True,(255,0,0))
 window = display.set_mode((700,500))
 display.set_caption("Пинг-Понг")
 BLUE = (0, 0, 255)
 game = True
+finish = False
 class GameSprite(sprite.Sprite):
     def __init__(self,player_image,player_x,player_y,player_speed,sprite_width,sprite_height):
         super().__init__()
@@ -31,18 +38,43 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys_pressed[K_DOWN] and self.rect.y < 420:
             self.rect.y += self.speed
-first_racket = Player('ракетка.png',30,250,1,20,70)
-second_racket = Player('ракетка.png',670,250,1,20,70)
-ball = GameSprite('мяч.jpg',350,250,3,50,50)
+first_racket = Player('ракетка.png',30.0,250.0,1.0,20,70)
+second_racket = Player('ракетка.png',670.0,250.0,1.0,20,70)
+ball = GameSprite('мяч.jpg',350.0,250.0,3,50,50)
+speed_x = 1
+speed_y = 1
 while game:
     window.fill(BLUE)
     events = event.get()
     for i in events:
         if i.type == QUIT:
             game = False
-    first_racket.reset()
-    first_racket.movement1()  
-    second_racket.reset()
-    second_racket.movement2()
-    ball.reset()
-    display.update()
+    if finish != True:
+        font11 = font.render("Счёт:"+str(count1),True,(255,255,255))
+        font22 = font.render("Счёт:"+str(count2),True,(255,255,255))
+        first_racket.reset()
+        first_racket.movement1()  
+        second_racket.reset()
+        second_racket.movement2()
+        ball.reset()
+        ball.rect.y += speed_y
+        ball.rect.x += speed_x
+        if ball.rect.y > 450.0:
+            speed_y*=-1
+        if ball.rect.y < 50.0:
+            speed_y*=-1
+        if sprite.collide_rect(first_racket,ball):
+            speed_x*=-1
+            count1+=1
+        if sprite.collide_rect(second_racket,ball):
+            count2+=1
+            speed_x*=-1
+        if ball.rect.x >= 650.0:
+            window.blit(font2,(250,250))
+            finish = True
+        if ball.rect.x <= 0.0:
+            window.blit(font1,(250,250))
+            finish = True
+        window.blit(font11,(20,30))
+        window.blit(font22,(580,30))
+        display.update()
